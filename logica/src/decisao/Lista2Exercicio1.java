3edd3b6c692245b33dfd791252fe47bd00849b69
Renato Alves <realvsp@gmail.com>
2021-05-04 17:28:58 -0300

    primeira e 2.as aulas

---
+++ logica/src/decisao/Lista2Exercicio1.java
@@ -0,0 +1,46 @@
+package decisao;
+
+import javax.swing.JOptionPane;
+
+public class Lista2Exercicio1 {
+
+	public static void main(String[] args) {
+
+		/*1. Um hotel cobra R$ 80,00 a diária e mais uma taxa de serviços. 
+		/ 	A taxa de serviços é de: 
+		/   R$ 5,50 por diária, se o número de diárias for maior que 15; 
+		/	R$ 6,00 por diária, se o número de diárias for igual a 15; 
+		/	R$ 8,00 por diária, se o número de diárias for menor que 15.
+		/ Monte uma aplicação que apresente a conta do cliente
+		*/
+
+		String cliente = JOptionPane.showInputDialog("Cliente: ");
+		short diarias = Short.parseShort(JOptionPane.showInputDialog("Qual o numero de diarias "));
+
+		double diaria1 = 5.50;
+		double diaria2 = 6.00;
+		double diaria3 = 8.00;
+
+		if(diarias <15) {
+			System.out.println("O valor de sua conta é R$ " + (diarias*8.00));
+		}
+		else if(diarias =15) {
+			System.out.println("O valor de sua conta é R$ " + (diarias*6.00));
+		}
+		else {
+			System.out.println("O valor de sua conta é R$ " + (diarias*5.50));
+		}
+
+
+
+		System.out.println(" Nome do Aluno.......: " + aluno);
+		System.out.println(" Disciplina   .......: " + disciplina);
+		System.out.println(" Media das notas.......: " + media);
+	}
+
+}
+
+
+
+
+
---
+++ logica/src/variaveis/TiposPrimitivos.java
@@ -0,0 +1,43 @@
+package variaveis;
+
+import javax.swing.JOptionPane;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		
+	
+		/*
+		 * TIPOS PRIMITIVOS: são tipos que estão dentro da linguagem
+		 * 
+		 * boolean = tipo lógico (true/false) utilizar somente em que a resposta será sempre sim ou não ( cliente ativo ??)
+		 * char = 1 caracter (usa como separador apóstrofos) 'a' (char) diferente de "a" (String)
+		 * byte = -128 / +127
+		 * short = 32 mil / + 32 mil
+		 * int = - 2 trilhões / + 2 trilhões
+		 * long = 9 quintilhões
+		 * float 
+		 * double = possui o dobro de precição em relação ao float.
+		 *  
+		 *  
+		 *  Classes Wrapers ( classes que servem para apoiar os tipos primitivos
+		 *  
+		 *  int => Integer
+		 *  float - > Float
+		 *  boolean -> Boolean
+		 *  double -> Double
+		 *  
+		 *  
+		 */
+		
+		
+		float salario = Float.parseFloat(JOptionPane.showInputDialog("Salario"));
+		float fgts = (float) (salario * 0.08); // ocorre o casting
+		int numero = 5;
+		byte result = (byte) (numero +2) ; // ocorre o casting
+		
+		
+		
+		
+	}
+	
+}
