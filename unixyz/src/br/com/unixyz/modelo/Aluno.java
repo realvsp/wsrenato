f4c7a6903226f96b4c4db85ab3a8307d75baa353
Renato Alves <realvsp@gmail.com>
2021-05-12 18:08:19 -0300

    commit 1205

--- unixyz/src/br/com/unixyz/modelo/Aluno.java
+++ unixyz/src/br/com/unixyz/modelo/Aluno.java
@@ -1,14 +1,13 @@
 package br.com.unixyz.modelo;
-
 /*
- * Design Pattern : compilado de padrões de projetos para OO.
- * Um dos padrões é o DTO (Data Transfer Object) cuja função é  
- * aplicar boa prática na transição entre o projeto e a programação.
+ * Design Pattern: compilado de padrões de projetos para OO.
+ * Um dos padrões é o DTO (Data Transfer Object) cuja função é aplicar
+ * boa prática na transição entre o projeto e a programação.
  * Sugestões:
- * 1-) TODOS atributos devem estar como privados
- * 2-) Cada atributo deve possuir um método set (para input) e outro método get (para output)
- * 
- * 
+ * 1º TODOS os atributos devem estar como privados.
+ * 2º Cada atributo deve possuir um método set e outro método get.
+ * 3º No MINIMO dois construtores, sendo um vazio e outro com 
+ * todos os atributos sendo preenchidos. (cheio)
  */
 
 public class Aluno {
@@ -18,38 +17,67 @@ public class Aluno {
 	private String email;
 	private String fone;
 	private String cpf;
+	private Endereco endereco;
 	
-	public String getUsuario () {
-		if (email.contains("@")) {
-		return email.substring (0, email.indexOf("@")) ;
+	public Endereco getEndereco() {
+		return endereco;
 	}
-		return email;
-}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 
+	@Override
+	public String toString() {
+		return "Aluno [rm=" + rm + ", nome=" + nome + ", email=" + email + ", fone=" + fone + ", cpf=" + cpf
+				+ ", endereco=" + endereco + "]";
+	}
+	public Aluno(int rm, String nome, String email, String fone, String cpf, Endereco endereco) {
+		super();
+		this.rm = rm;
+		this.nome = nome;
+		this.email = email;
+		this.fone = fone;
+		this.cpf = cpf;
+		this.endereco = endereco;
+	}
+
+	public Aluno() {}
 	
-	public String getResumo () {
+	public Aluno (int rm) {
+		this.rm = rm;
+	}
+	public Aluno(int rm, String nome) {
+		this.rm = rm;
+		this.nome=nome;
+	}
+	
+	public String getUsuario() {
+		if (email.contains("@")) {
+			return email.substring(0, email.indexOf("@"));
+		}
+		return email;
+	}
+	
+	public String getResumo() {
 		return 
-				"Nome:  " + nome + "\n" +
-				"RM  :  " +rm ;
+				"Nome: " + nome + "\n" + 
+				"RM: " + rm;
 	}
 	
 	public void setBasico(String nome, int rm) {
 		this.nome = nome;
-		this.rm = rm;
+		this.rm=rm;
 	}
-	
-	@Override 
-	public String toString() {
-		return "Aluno [rm=" + rm + ", nome=" + nome + ", email=" + email + ", fone=" + fone + ", cpf=" + cpf + "]";
-	}
-	
-	
+
+
 	public int getRm() {
 		return rm;
 	}
+		
 	public void setRm(int rm) {
 		this.rm = rm;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
@@ -75,8 +103,10 @@ public class Aluno {
 		this.cpf = cpf;
 	}
 	
+	
+	
 
-
-
+	
+	
+	
 }
-
